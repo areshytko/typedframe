1b8b20c3341ae754c5fe21b6981757cfa5212f15
Alexander Reshytko <alexander@reshytko.com>
2021-11-24 14:35:59 +0400

    add utc datetime to interface

--- typedframe/__init__.py
+++ typedframe/__init__.py
@@ -1,3 +1,3 @@
-from typedframe.typedframe import TypedDataFrame, DATE_TIME_DTYPE, STRING_DTYPE
+from typedframe.typedframe import TypedDataFrame, DATE_TIME_DTYPE, UTC_DATE_TIME_DTYPE, STRING_DTYPE
 
-__version__ = '0.5.0'
+__version__ = '0.5.1'
