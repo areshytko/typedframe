493b31a0818f771a751d838af3c70c409fdfea1f
Alexander Reshytko <alexander@reshytko.com>
2023-01-18 17:47:20 +0400

    v0.8.0: refactor into base and pandas typedrame

---
+++ typedframe/base.py
@@ -0,0 +1,94 @@
+"""
+Basic classes for typed wrappers over dataframes
+"""
+from abc import abstractmethod
+from itertools import chain
+from typing import Type, TypeVar, Any
+
+import pandas as pd
+
+
+T = TypeVar("T", bound="TypedDataFrameBase")
+
+
+class TypedDataFrameBase:
+    """
+    Wrapper class over DataFrame to provide explicit schema specs.
+
+    Provide expected dataframe schema in schema static variable.
+    Provide optional columns in optional static variable.
+    All columns from optional schema that are missing in a wrapped dataframe will be added with NaN values.
+
+    Schemas can be inheritted via Python class inheritance. The semantics of it is the following:
+    all columns of the parent are also included to a child schema.
+    """
+
+    schema = {}
+
+    optional = {}
+
+    @classmethod
+    @abstractmethod
+    def convert(cls: Type[T], df: pd.DataFrame) -> T:
+        pass
+
+    @classmethod
+    def dtype(cls: Type[T], with_optional: bool = True) -> dict:
+        """
+        Combines schema of a current class and all super classes
+        """
+        return dict(chain(*(chain(cls.schema.items(), cls.optional.items())
+                            if with_optional else cls.schema.items()
+                            for cls in cls.__mro__[:-1] if hasattr(cls, 'schema'))))
+
+    @classmethod
+    def _extract_actual_dtypes(cls: Type[T], df: pd.DataFrame) -> dict:
+        return cls._extract_actual_dtypes(df)
+
+    @classmethod
+    @abstractmethod
+    def _normalize_actual_dtype(cls: Type[T], dtype: Any) -> Any:
+        pass
+
+    @classmethod
+    @abstractmethod
+    def _normalize_expected_dtype(cls: Type[T], dtype: Any) -> Any:
+        pass
+
+    @classmethod
+    def _dtypes_mismatch(cls: Type[T], actual: Any, expected: Any) -> bool:
+        actual = cls._normalize_actual_dtype(actual)
+        expected = cls._normalize_expected_dtype(expected)
+        return actual != expected
+
+    def __init__(self, df: pd.DataFrame):
+
+        if not isinstance(df, pd.DataFrame):
+            raise AssertionError(f"Input argument of type {type(df)} is not an instance of pandas DataFrame")
+
+        actual_dtypes = self._extract_actual_dtypes(df)
+        expected = self.dtype(with_optional=False).items()
+
+        diff = set()
+        for col, dtype in expected:
+            try:
+                if col not in actual_dtypes or self._dtypes_mismatch(actual_dtypes[col], dtype):
+                    diff.add((col, dtype))
+            except TypeError:
+                diff.add((col, dtype))
+
+        optional = self.dtype().items()
+        for col, dtype in optional:
+            try:
+                if col in actual_dtypes and self._dtypes_mismatch(actual_dtypes[col], dtype):
+                    diff.add((col, dtype))
+            except TypeError:
+                diff.add((col, dtype))
+
+        if diff:
+            actual = {key: self._normalize_actual_dtype(value) for key, value in df.dtypes.to_dict().items()}
+            expected = {key: self._normalize_expected_dtype(value) for key, value in self.dtype().items()}
+            raise AssertionError(
+                "Dataframe doesn't match schema\n"
+                f"Actual: {actual}\nExpected: {expected}\nDifference: {diff}"
+            )
---
+++ typedframe/pandas_typedframe.py
@@ -0,0 +1,125 @@
+
+from typing import Type, TypeVar, Any
+import pytz
+
+import pandas as pd
+import numpy as np
+
+from typedframe.base import TypedDataFrameBase
+
+try:
+    from pandas.api.types import CategoricalDtype
+except ImportError:
+    from pandas.types.dtypes import CategoricalDtype
+
+"""
+dtype for datetime column
+"""
+DATE_TIME_DTYPE = np.dtype('datetime64[ns]')
+UTC_DATE_TIME_DTYPE = pd.DatetimeTZDtype('ns', pytz.UTC)
+
+T = TypeVar("T", bound="PandasTypedDataFrame")
+
+_OBJECT_TYPES = {list, str, dict}
+
+
+class PandasTypedDataFrame(TypedDataFrameBase):
+    """
+    Wrapper class over pandas
+    """
+
+    index_schema = (None, None)  # (name, dtype)
+
+    @classmethod
+    def convert(cls: Type[T], df: pd.DataFrame) -> T:
+        """
+                Tries to convert a given dataframe and wrap in a typed dataframe.
+
+                Examples
+                --------
+
+                >>> from typedframe import TypedDataFrame, DATE_TIME_DTYPE
+                >>> class MyTable(TypedDataFrame):
+                ...    schema = {
+                ...       "col1": str,
+                ...       "col2": np.int32,
+                ...       "col3": ('foo', 'bar')
+                ...    }
+                ...    optional = {
+                ...       "col4": bool,
+                ...       "col5": DATE_TIME_DTYPE
+                ...    }
+
+                >>> df = pd.DataFrame({"col1": ['foo'], "col2": np.array([1], dtype=np.int32), "col3": ['bar']})
+                >>> df.col3 = pd.Categorical(df.col3, categories=('foo', 'bar'), ordered=True)
+                >>> print(MyTable.convert(df).df)
+                """
+        df = df.copy()
+        expected = cls.dtype()
+        for col in df.columns:
+            if col in expected:
+                if isinstance(expected[col], tuple):
+                    actual_cats = set(df[col].unique())
+                    categories_diff = actual_cats.difference(set(expected[col]))
+                    if categories_diff:
+                        raise AssertionError(f"For column: {col} there are unknown categories: {categories_diff}")
+                    df[col] = pd.Categorical(df[col], categories=expected[col], ordered=True)
+                elif expected[col] == DATE_TIME_DTYPE:
+                    df[col] = pd.to_datetime(df[col])
+                elif expected[col] == UTC_DATE_TIME_DTYPE:
+                    df[col] = pd.to_datetime(df[col], utc=True)
+                else:
+                    df[col] = df[col].astype(expected[col])
+
+        if cls.index_schema[1]:
+            df.index = df.index.astype(cls.index_schema[1])
+            df.index.name = cls.index_schema[0]
+
+        return cls(df)
+
+    @classmethod
+    def _extract_actual_dtypes(cls: Type[T], df: pd.DataFrame) -> dict:
+        return df.dtypes.to_dict()
+
+    @classmethod
+    def _normalize_actual_dtype(cls: Type[T], dtype: Any) -> Any:
+        if isinstance(dtype, CategoricalDtype):
+            return tuple(dtype.categories)
+        else:
+            return dtype
+
+    @classmethod
+    def _normalize_expected_dtype(cls: Type[T], dtype: Any) -> Any:
+        try:
+            if dtype in _OBJECT_TYPES:
+                return object
+            else:
+                return dtype
+        except TypeError:
+            return dtype
+
+    def __init__(self, df: pd.DataFrame):
+        super().__init__(df)
+
+        if not isinstance(df, pd.DataFrame):
+            raise AssertionError(f"Input argument of type {type(df)} is not an instance of pandas DataFrame")
+
+        if self.index_schema[1]:
+            if df.index.name != self.index_schema[0]:
+                raise AssertionError(f"expected index name {self.index_schema[0]}, actual index name {df.index.name}")
+            try:
+                if self._dtypes_mismatch(df.index.dtype, self.index_schema[1]):
+                    raise AssertionError(f"expected index dtype {self.index_schema[1]}, actual index dtype {df.index.dtype}")
+            except TypeError:
+                raise AssertionError(f"expected index dtype {self.index_schema[1]}, actual index dtype {df.index.dtype}")
+
+        categoricals = [df[c] for c in df.columns if isinstance(df[c].dtype, CategoricalDtype)]
+        for col in categoricals:
+            if object != col.values.categories.dtype:
+                raise AssertionError("Categoricals must have str categories")
+            if np.nan in col.unique():
+                raise AssertionError("Categoricals must not have NaNs")
+
+        addon = {col: dtype for col, dtype in self.dtype().items() if col not in df.columns}
+        self.df: pd.DataFrame = df if len(addon) == 0 else pd.concat(
+            [df, pd.DataFrame(columns=addon.keys()).astype(addon)], axis=1)
